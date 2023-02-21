a5a335b8544ebe2a51bd0adbf81a839a1616c0f2
Anastasia <dianastasiavi@gmail.com>
2023-02-21 12:08:02 +0200

    bookScreenVM now is able to load image even if wasn't loaded in cell

--- Bookshelf/Bookshelf/Screens/BookScreen/BookScreenViewController.swift
+++ Bookshelf/Bookshelf/Screens/BookScreen/BookScreenViewController.swift
@@ -47,11 +47,12 @@ class BookScreenViewController: UIViewController, StoryboardLoadable {
     
     private func setBookData() {
         self.name?.text = self.viewModel.book.title
-        self.cover?.image = self.viewModel.cover.value.image
+        self.cover?.image = self.viewModel.cover.value
         self.author?.text = self.viewModel.book.author
         self.publishYear?.text = "Publish year: \(self.viewModel.book.publishYear)"
         self.numberOfPages?.text = "Number of pages: \(self.viewModel.book.numberOfPages)"
         self.firstSentense?.text = self.viewModel.book.firstSentense
+        self.firstSentense?.contentMode = .top
     }
 
     //MARK: -
--- Bookshelf/Bookshelf/Screens/BookScreen/BookScreenViewModel.swift
+++ Bookshelf/Bookshelf/Screens/BookScreen/BookScreenViewModel.swift
@@ -15,16 +15,18 @@ class BookScreenViewModel {
     //MARK: -
     //MARK: Properties
     
-    var cover = BehaviorRelay<UIImageView>(value: UIImageView())
+    var cover = BehaviorRelay<UIImage>(value: UIImage())
     var disposeBag: DisposeBag = .init()
     var book: BookFound
     
     init(book: BookFound) {
         self.book = book
-        guard let coverData = book.coverId,
-        let url = URL(string: "https://covers.openlibrary.org/b/id/" + "\(coverData)" + ".jpg")
-        else { return }
-        self.cover.value.af.setImage(withURL: url, placeholderImage: UIImage(named: "loading"), imageTransition: .crossDissolve(5.0))
+        guard let url = URL(string: "https://covers.openlibrary.org/b/id/" + "\(book.coverId)" + ".jpg"),
+              let data = try? Data(contentsOf: url),
+              let image = UIImage(data: data)
+        else {
+            return
+        }
+        self.cover.accept(image)
     }
-    
 }
